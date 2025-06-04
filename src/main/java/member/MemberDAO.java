3d716e73e9f9669fd21c0f8ae5209242310f315e
user <tmdrnf35@g.shingu.ac.kr>
2025-06-04 16:07:39 +0900

    회원정보관리예제 중간

---
+++ src/main/java/member/MemberDAO.java
@@ -0,0 +1,63 @@
+package member;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberDAO {
+	
+	Connection conn = null;
+	PreparedStatement pstmt;
+	
+	final String JDBC_DRIVER = "org.h2.Driver" ;
+	final String JDBC_URL = "jdbc:h2:tcp://localhost/~/test";
+	
+	public void open() {
+		try {
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(JDBC_URL, "sa", "");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void close() {
+		try {
+			pstmt.close();
+			conn.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public List<Member> getAll() {
+		List<Member> memberlist = new ArrayList<>();
+		//모든 회원들의 정보를 리턴하는 부분
+		open();
+		try {
+			pstmt = conn.prepareStatement("SELECT * FROM members");
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+			Member m = new Member();
+			m.setId(rs.getInt("id"));
+			m.setUsername(rs.getString("username"));
+			m.setCompany(rs.getString("comapny"));
+			m.setBirthday(rs.getDate("birtday"));
+			m.setEmail(rs.getString("email"));
+			
+			memberlist.add(m);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return memberlist;
+	}
+	public void insert() {
+		
+	}
+	
+}
